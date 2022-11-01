011d66c5f8668968b9e3785d9ecbf121821e34ee
ThiHaZaw <thzaw4321@gmail.com>
2022-11-01 12:57:15 +0630

    Update

---
+++ Course.js
@@ -0,0 +1,42 @@
+ const CourseImg = [
+    {
+        id: 1,
+        image : "./images/one.jpg"
+    },
+    {
+        id: 2,
+        image : "./images/two.jpg"
+    },
+    {
+        id: 3,
+        image : "./images/three.jpg"
+    },
+    {
+        id: 4,
+        image : "./images/four.jpg"
+    },
+    {
+        id: 5,
+        image : "./images/five.jpg"
+    },
+    {
+        id: 6,
+        image : "./images/six.jpg"
+    },
+    {
+        id: 7,
+        image : "./images/seven.jpg"
+    },
+    {
+        id: 8,
+        image : "./images/1015293_OJ91BD0.jpg"
+    },
+    {
+        id: 9,
+        image : "./images/10782951_19199360.jpg"
+    },
+    {
+        id: 10,
+        image : "./images/12063795_4884785.jpg"
+    }
+]
--- main.js
+++ main.js
@@ -175,3 +175,4 @@ UserSignIn.addEventListener("click", () => {
   })
 })
 
+
